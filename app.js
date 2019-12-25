cd822440022ac4d8f987c4e5bc522a9191c29778
Josh Justice <josh.justice@accesscfa.com>
2019-12-25 09:48:36 -0500

    Extract operative module

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
 var cors = require('cors');
+const Operative = require('./operative');
 const db = require('./models');
 const { Op } = require('sequelize');
 const { Todo, Operation } = db;
@@ -29,81 +30,13 @@ const repo = {
 let app = express();
 const httpServer = http.createServer(app);
 
-const getOperationsSince = since => repo.findOperationsSince(since);
-
-const getDatabaseRoute = (req, res) => {
-  repo
-    .findAllRecords()
-    .then(todos => res.send(todos))
-    .catch(err => res.send(err));
-};
-
-const getOperationsRoute = (req, res) => {
-  getOperationsSince(req.query.since)
-    .then(operations => res.send(operations))
-    .catch(err => res.send(err));
-};
-
-const handleOperations = async operations => {
-  for (const operation of operations) {
-    await repo.recordOperation(operation);
-
-    switch (operation.action) {
-      case 'create': {
-        const attributesWithId = Object.assign(
-          { id: operation.recordId },
-          operation.attributes
-        );
-        await repo.createRecord(attributesWithId);
-        break;
-      }
-      case 'update':
-        await repo.updateRecord(operation.recordId, operation.attributes);
-        break;
-      case 'delete':
-        await repo.destroyRecord(operation.recordId);
-        break;
-    }
-  }
-};
-
-const postOperationsRoute = async (req, res) => {
-  const operations = req.body;
-
-  // this should NOT yet include the operation sent into us
-  const otherOperations = await getOperationsSince(req.query.since);
-
-  handleOperations(operations);
-
-  res.send(otherOperations);
-};
-
-const todoRouter = express.Router();
-todoRouter.route('/').get(getDatabaseRoute);
-todoRouter
-  .route('/operations')
-  .get(getOperationsRoute)
-  .post(express.json(), postOperationsRoute);
+const operative = Operative.create({ repo });
 
 app.use(cors());
-app.use('/todos', todoRouter);
+app.use('/todos', operative.router());
 
 const wss = new WebSocket.Server({ server: httpServer });
-
-const clientsOtherThan = me =>
-  Array.from(wss.clients).filter(
-    client => client !== me && client.readyState === WebSocket.OPEN
-  );
-
-wss.on('connection', conn => {
-  conn.on('message', message => {
-    const operations = JSON.parse(message);
-    handleOperations(operations);
-    clientsOtherThan(conn).forEach(client => {
-      client.send(message);
-    });
-  });
-});
+operative.configureWss(wss);
 
 const port = process.env.PORT || 3000;
 httpServer.listen(port, () => {
